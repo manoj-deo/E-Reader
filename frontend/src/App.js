cac91e583857d1a54d8ca693b576f8baf376fcc2
Manoj Deo <manoj.deo91@gmail.com>
2025-02-08 00:27:04 -0500

    Homepage initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from './HomePage'; // adjust the path as needed
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
