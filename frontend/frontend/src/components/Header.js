99ee37bc5edc789ae893d7b5b4c52c425e04039a
Manoj Deo <manoj.deo91@gmail.com>
2025-02-09 18:52:37 -0500

    Added AboutPage component

---
+++ frontend/frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+// src/components/Header.js
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          E-Reader
+        </Typography>
+        <Button color="inherit" component={Link} to="/">
+          Home
+        </Button>
+        <Button color="inherit" component={Link} to="/login">
+          Login
+        </Button>
+        <Button color="inherit" component={Link} to="/about">
+          About
+        </Button>
+        <Button color="inherit" component={Link} to="/contact">
+          Contact
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
